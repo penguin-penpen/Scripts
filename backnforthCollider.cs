d89a5514b1d07a682909a14f22a390545fe5d20c
penguin-penpen <penguingohack@outlook.com>
2016-02-13 23:12:49 +0800

    在玩家与障碍物自身初始坐标间来回移动

---
+++ backnforthCollider.cs
@@ -0,0 +1,28 @@
+﻿/*
+ * 来回移动的障碍，碰撞即死
+ * 在farEnd和自身transform之间来回移动
+ */
+using UnityEngine;
+using System.Collections;
+public class backnforthCollider : MonoBehaviour
+{
+	public Transform farEnd;
+	private Vector3 frometh;
+	private Vector3 untoeth;
+	private float secondsForOneLength = 1f;
+
+	void Start()
+	{
+		farEnd = GameObject.Find ("Player").transform;
+		frometh = transform.position;
+		untoeth = farEnd.position;
+	}
+
+	void Update()
+	{
+		transform.position = Vector3.Lerp(frometh, untoeth, //基于smoothstep返回值而返回frometh与untoeth间的值
+			Mathf.SmoothStep(0f,1f,
+				Mathf.PingPong(Time.time/secondsForOneLength, 1f) //在0到1间往返
+			) );
+	}
+}
